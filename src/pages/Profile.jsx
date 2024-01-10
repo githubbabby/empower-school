0359119ee55fc91c94a1c6dd0400e3299b800500
githubbabby <githubbaby@hotmail.com>
2024-01-10 04:39:05 -0300

    create edit profile functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut, updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { db } from "../firebase.config";
+import { doc, updateDoc } from "firebase/firestore";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -16,6 +19,30 @@ export default function Profile() {
       signOut(auth);
       toast.success("Sesion cerrada con exito");
       navigate("/sign-in");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+        toast.success("Perfil actualizado con exito");
+      } else {
+        toast.info("No se ha realizado ningun cambio");
+      }
     } catch (error) {
       toast.error(error.message);
     }
@@ -33,8 +60,14 @@ export default function Profile() {
               type="text"
               id="name"
               value={name}
-              disabled
-              className="mb-6 w-full rounded border border-gray-300 bg-white px-4 py-2 text-xl text-gray-700 transition ease-in-out hover:cursor-not-allowed"
+              disabled={!isEditing}
+              onChange={onChange}
+              className={
+                "mb-6 w-full rounded border px-4 py-2 text-xl text-gray-700 transition ease-in-out" +
+                (isEditing
+                  ? " cursor-text border-gray-300 bg-white"
+                  : " border-transparent bg-gray-200")
+              }
             />
 
             {/* Email */}
@@ -43,13 +76,19 @@ export default function Profile() {
               id="email"
               value={email}
               disabled
-              className="mb-6 w-full rounded border border-gray-300 bg-white px-4 py-2 text-xl text-gray-700 transition ease-in-out hover:cursor-not-allowed"
+              className="mb-6 w-full rounded border border-gray-300 bg-white px-4 py-2 text-xl text-gray-700 transition ease-in-out"
             />
             <div className="mb-6 flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="flex items-center">
-                Quiere cambiar su nombre?{" "}
-                <span className="ml-1 text-red-700 transition duration-300 ease-in-out hover:cursor-pointer hover:text-red-900">
-                  Editar
+                Quiere editar su perfil?{" "}
+                <span
+                  onClick={() => {
+                    isEditing && onSubmit();
+                    setIsEditing((prevState) => !prevState);
+                  }}
+                  className="ml-1 text-red-700 transition duration-300 ease-in-out hover:cursor-pointer hover:text-red-900"
+                >
+                  {isEditing ? "Guardar" : "Editar"}
                 </span>
               </p>
               <p
