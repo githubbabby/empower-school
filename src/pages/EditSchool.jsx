4b4395db1a606968e6ff828339f4b4bb2871932f
githubbabby <githubbaby@hotmail.com>
2024-05-07 08:22:12 -0400

    fixed logic for adding new institutes and modifying existing institutes in EditSchool. Delete institutes logic next

--- src/pages/EditSchool.jsx
+++ src/pages/EditSchool.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
-import { doc, getDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  addDoc,
+  serverTimestamp,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 import { storeImage } from "../imageUtils";
 import { getAuth } from "firebase/auth";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import Spinner from "../components/Spinner";
 import { useParams, useNavigate } from "react-router-dom";
+import _ from "lodash";
 
 export default function EditSchool() {
   const navigate = useNavigate();
@@ -33,6 +42,9 @@ export default function EditSchool() {
     imagenes,
   } = formData;
 
+  const [institutes, setInstitutes] = useState([{}]);
+  const [originalInstitutes, setOriginalInstitutes] = useState([]);
+
   const params = useParams();
 
   useEffect(() => {
@@ -56,6 +68,22 @@ export default function EditSchool() {
           longitud: data.longitud,
         });
         setMarkerPosition([data.latitud, data.longitud]);
+
+        const institutesRef = collection(
+          db,
+          "escuelas",
+          params.schoolId,
+          "institutos"
+        );
+        const instituteSnapshot = await getDocs(institutesRef);
+        if (instituteSnapshot.docs.length > 0) {
+          const instituteList = instituteSnapshot.docs.map((doc) => ({
+            uid: doc.id,
+            ...doc.data(),
+          }));
+          setInstitutes(instituteList);
+          setOriginalInstitutes(instituteList);
+        }
       } else {
         toast.error("No se encontro la escuela");
       }
@@ -69,6 +97,25 @@ export default function EditSchool() {
     if (e.target.files) {
       setFormData({ ...formData, imagenes: e.target.files });
     }
+  };
+
+  const handleAddInstitute = () => {
+    setInstitutes([...institutes, {}]);
+  };
+
+  const handleRemoveInstitute = (index) => {
+    const newInstitutes = [...institutes];
+    newInstitutes.splice(index, 1);
+    setInstitutes(newInstitutes);
+  };
+
+  const handleInstituteChange = (index, event) => {
+    const newInstitutes = [...institutes];
+    newInstitutes[index] = {
+      ...newInstitutes[index],
+      [event.target.name]: event.target.value,
+    };
+    setInstitutes(newInstitutes);
   };
 
   const [markerPosition, setMarkerPosition] = useState([latitud, longitud]);
@@ -119,12 +166,64 @@ export default function EditSchool() {
     try {
       const docRef = doc(db, "escuelas", params.schoolId);
       await updateDoc(docRef, formDataCopy);
+      const institutesRef = collection(
+        db,
+        "escuelas",
+        params.schoolId,
+        "institutos"
+      );
+      const batch = [];
+      institutes.forEach((institute) => {
+        try {
+          if (institute.uid) {
+            const instituteRef = doc(
+              db,
+              "escuelas",
+              params.schoolId,
+              "institutos",
+              institute.uid
+            );
+            const index = originalInstitutes.findIndex(
+              (originalInstitute) => originalInstitute.uid === institute.uid
+            );
+
+            if (!_.isEqual(originalInstitutes[index], institute)) {
+              const { uid, ...instituteCopy } = institute;
+              batch.push(
+                updateDoc(instituteRef, {
+                  ...instituteCopy,
+                  fecha_modificacion: serverTimestamp(),
+                })
+              );
+            }
+          } else {
+            batch.push(
+              addDoc(institutesRef, {
+                ...institute,
+                fecha_creacion: serverTimestamp(),
+                id_usuario: auth.currentUser.uid,
+              })
+            );
+          }
+        } catch (error) {
+          console.error(
+            `Error processing institute: ${institute.uid || "new"}`,
+            error
+          );
+        }
+      });
+
+      try {
+        await Promise.all(batch);
+      } catch (error) {
+        console.error("Error executing batch", error);
+      }
+
       setLoading(false);
       toast.success("Escuela actualizada correctamente!");
       navigate(`/edit-school/${docRef.id}`);
     } catch (error) {
       setLoading(false);
-      console.error(error);
       toast.error(error.message);
     }
   }
@@ -133,7 +232,7 @@ export default function EditSchool() {
     return <Spinner />;
   }
   return (
-    <main className="mx-auto max-w-md px-2">
+    <main className="mx-auto px-2 md:max-w-3xl lg:max-w-5xl">
       <h1 className="mt-6 text-center text-3xl font-bold">Editar escuela</h1>
       <form onSubmit={handleSubmit}>
         {/* Nombre */}
@@ -147,6 +246,50 @@ export default function EditSchool() {
           minLength={3}
           className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
+        {institutes &&
+          institutes.length > 0 &&
+          institutes.map((institute, index) => (
+            <div key={index}>
+              <div className="mt-6 rounded-lg bg-white px-9 py-6 shadow-lg">
+                <div className="mx-auto flex max-w-6xl items-center justify-between">
+                  <p className="text-lg font-semibold">Nombre del Instituto</p>
+                  <button
+                    className="rounded-md bg-red-700 px-3 py-2 text-white focus:bg-red-500 focus:outline-none"
+                    type="button"
+                    onClick={() => handleRemoveInstitute(index)}
+                  >
+                    Remover Institucion
+                  </button>
+                </div>
+                <input
+                  type="text"
+                  name="nombre_instituto"
+                  value={institute.nombre_instituto}
+                  onChange={(event) => handleInstituteChange(index, event)}
+                  minLength={3}
+                  required
+                  className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <p className="mt-6 text-lg font-semibold">Turno</p>
+                <input
+                  type="text"
+                  name="turno"
+                  value={institute.turno}
+                  onChange={(event) => handleInstituteChange(index, event)}
+                  minLength={3}
+                  className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+            </div>
+          ))}
+
+        <button
+          className="mt-9 rounded-md bg-green-700 px-3 py-2 text-white focus:bg-green-500 focus:outline-none"
+          type="button"
+          onClick={handleAddInstitute}
+        >
+          Añadir Institucion
+        </button>
         {/* Direccion */}
         <p className="mt-6 text-lg font-semibold">Direccion de la escuela</p>
         <input
