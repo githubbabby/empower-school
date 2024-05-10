685170fdd4b6e3ab3ffdee94557ea74e30c9b0bd
githubbabby <githubbaby@hotmail.com>
2024-05-10 03:02:13 -0400

    CreateSchool field changes

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import AsyncSelect from "react-select/async";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import {
@@ -10,12 +9,10 @@ import {
   serverTimestamp,
   where,
 } from "firebase/firestore";
-import { storeImage } from "../imageUtils";
 import { getAuth } from "firebase/auth";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import { useMap } from "react-leaflet";
 import Spinner from "../components/Spinner";
 import { useNavigate } from "react-router-dom";
+import Select from "react-select";
 
 export default function CreateListing() {
   const navigate = useNavigate();
@@ -24,13 +21,7 @@ export default function CreateListing() {
 
   const [listingData, setListingData] = useState({
     nombre: "",
-    direccion: "",
-    distrito: "",
-    departamento: "",
-    barrio: "",
-    latitud: -25.306166036627506,
-    longitud: -57.53814697265626,
-    imagenes: [],
+    observacion: "",
   });
   const [listingItems, setListingItems] = useState([{}]);
 
@@ -50,16 +41,7 @@ export default function CreateListing() {
     setListingItems(newListingItems);
   };
 
-  const {
-    nombre,
-    direccion,
-    distrito,
-    departamento,
-    barrio,
-    latitud,
-    longitud,
-    imagenes,
-  } = listingData;
+  const { nombre, observacion } = listingData;
 
   const loadOptions = async (inputValue) => {
     try {
@@ -117,49 +99,15 @@ export default function CreateListing() {
     }
   };
 
-  const [markerPosition, setMarkerPosition] = useState([latitud, longitud]);
-
-  const handleMarkerDragEnd = (e) => {
-    const { lat, lng } = e.target.getLatLng();
-    setMarkerPosition([lat, lng]);
-    setListingData({ ...listingData, latitud: lat, longitud: lng });
-  };
-
-  function MyMap({ center }) {
-    const map = useMap();
-
-    React.useEffect(() => {
-      map.flyTo(center);
-    }, [center, map]);
-
-    return null;
-  }
-
   async function handleSubmit(e) {
     e.preventDefault();
     setLoading(true);
 
-    if (imagenes.length > 6) {
-      setLoading(false);
-      toast.error("Solo se pueden subir 6 imagenes");
-      return;
-    }
-
-    const imgUrls = await Promise.all(
-      Array.from(imagenes).map((imagen) => storeImage("pedidos", imagen))
-    ).catch((error) => {
-      setLoading(false);
-      console.error(error);
-      return;
-    });
-
     const listingDataCopy = {
       ...listingData,
-      imgUrls,
       id_usuario: auth.currentUser.uid,
       fecha_creacion: serverTimestamp(),
     };
-    delete listingDataCopy.imagenes;
 
     try {
       const docRef = await addDoc(collection(db, "pedidos"), listingDataCopy);
@@ -202,12 +150,25 @@ export default function CreateListing() {
           minLength={3}
           className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-
-        {listingItems.map((listingItem, index) => (
-          <div key={index}>
-            <div className="mt-6 rounded-lg bg-white px-9 py-6 shadow-lg">
+        {/* Observacion */}
+        <p className="mt-6 text-lg font-semibold">Observacion</p>
+        <textarea
+          id="observacion"
+          value={observacion}
+          onChange={onChange}
+          minLength={3}
+          className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        {/* Articulos */}
+        <p className="mt-6 text-center text-2xl font-semibold">Articulos</p>
+        <div className="gap-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
+          {listingItems.map((listingItem, index) => (
+            <div
+              key={index}
+              className="mt-6 rounded-lg bg-white px-9 py-6 shadow-lg"
+            >
               <div className="mx-auto flex max-w-6xl items-center justify-between">
-                <p className="text-lg font-semibold">Nombre del Articulo</p>
+                <p className="text-lg font-semibold">Nombre</p>
                 <button
                   className="rounded-md bg-red-700 px-3 py-2 text-white focus:bg-red-500 focus:outline-none"
                   type="button"
@@ -218,25 +179,61 @@ export default function CreateListing() {
               </div>
               <input
                 type="text"
-                name="nombre_item"
-                value={listingItem.nombre_item || ""}
+                name="nombre_articulo"
+                value={listingItem.nombre_articulo || ""}
                 onChange={(event) => handleListingItemChange(index, event)}
                 minLength={3}
                 required
                 className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-              <p className="mt-6 text-lg font-semibold">Turno</p>
-              <input
-                type="text"
-                name="turno"
-                value={listingItem.turno || ""}
+              <div className="mx-auto flex max-w-6xl items-center space-x-4">
+                <div className="flex-1">
+                  <p className="mt-6 text-lg font-semibold">Cantidad</p>
+                  <input
+                    type="number"
+                    name="cantidad"
+                    value={listingItem.cantidad || ""}
+                    onChange={(event) => handleListingItemChange(index, event)}
+                    min={1}
+                    required
+                    className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+
+                <div className="flex-1">
+                  <p className="mt-6 text-lg font-semibold">Categoria</p>
+                  <Select
+                    name="categoria"
+                    value={listingItem.categoria || ""}
+                    onChange={(selectedOption) =>
+                      handleListingItemChange(index, {
+                        target: {
+                          name: "categoria",
+                          value: selectedOption,
+                        },
+                      })
+                    }
+                    options={[
+                      { value: "alimentos", label: "Alimentos" },
+                      { value: "ropa", label: "Ropa" },
+                      { value: "medicamentos", label: "Medicamentos" },
+                      { value: "otros", label: "Otros" },
+                    ]}
+                    className="mt-6 w-full"
+                  />
+                </div>
+              </div>
+              <p className="mt-6 text-lg font-semibold">Observacion</p>
+              <textarea
+                name="observacion"
+                value={listingItem.observacion || ""}
                 onChange={(event) => handleListingItemChange(index, event)}
                 minLength={3}
                 className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
         <button
           className="mt-9 rounded-md bg-green-700 px-3 py-2 text-white focus:bg-green-500 focus:outline-none"
@@ -245,88 +242,6 @@ export default function CreateListing() {
         >
           Añadir Articulo
         </button>
-
-        {/* Direccion */}
-        <p className="mt-6 text-lg font-semibold">Direccion del Pedido</p>
-        <input
-          type="text"
-          id="direccion"
-          value={direccion}
-          required
-          onChange={onChange}
-          minLength={3}
-          className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        {/* Distrito */}
-        <p className="mt-6 text-lg font-semibold">Ciudad</p>
-        <AsyncSelect
-          type="select"
-          id="distrito"
-          value={distrito.label}
-          onChange={handleDistritoChange}
-          defaultOptions
-          loadOptions={loadOptions}
-          required
-          className="mt-6"
-        />
-        {/* Departamento */}
-        <p className="mt-6 text-lg font-semibold">Departamento</p>
-        <input
-          type="text"
-          id="departamento"
-          value={departamento}
-          readOnly
-          className="mt-6 w-full rounded-md border border-gray-300 bg-gray-200 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-500"
-        />
-        {/* Barrio */}
-        <p className="mt-6 text-lg font-semibold">Barrio</p>
-        <input
-          type="text"
-          id="barrio"
-          value={barrio}
-          onChange={onChange}
-          minLength={3}
-          className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        {/* Mapa */}
-        <p className="mt-6 text-lg font-semibold">
-          Por favor marque el Pedido en el mapa (arrastre el marcador
-          manteniendo apretado el boton izquierdo del mouse para ubicar el
-          Pedido exactamente en el mapa. Puede usar la rueda del mouse para
-          hacer zoom en el mapa.)
-        </p>
-        <div className="mt-6 h-96 w-full rounded-md bg-gray-300">
-          <MapContainer
-            center={markerPosition}
-            zoom={10}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-            />
-            <Marker
-              draggable={true}
-              position={markerPosition}
-              eventHandlers={{
-                dragend: handleMarkerDragEnd,
-              }}
-            />
-            <MyMap center={markerPosition} />
-          </MapContainer>
-        </div>
-        {/* Imagenes */}
-        <div className="mb-6">
-          <p className="text-lg font-semibold">Imagenes del Pedido</p>
-          <input
-            type="file"
-            id="imagenes"
-            onChange={onChange}
-            multiple
-            accept=".jpg, .jpeg, .png"
-            className="mt-6 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
 
         {/* Submit */}
         <button
